f5a57608ef7ef2379d2087c97412f59599abeb80
Xavier Ochoa <xavier.ochoa@gmail.com>
2022-02-23 11:01:35 -0500

    Change location of praat scripts

--- application/analyzer/extraction/AudioExtractor.py
+++ application/analyzer/extraction/AudioExtractor.py
@@ -52,7 +52,8 @@ class AudioExtractor:
             mean_Hz = parselmouth.praat.call(pitch, "Get mean", 0, 0, "Hertz")
             stdev_Hz = parselmouth.praat.call(pitch, "Get standard deviation", 0, 0, "Hertz")
             variation = stdev_Hz / mean_Hz
-            sourcerun = "praat/syllablenucleiv3.praat"
+            praatPath=os.path.join(self.path, "..","..","praat")
+            sourcerun = os.path.join(praatPath,"praatsyllablenucleiv3.praat")
             objects = run_file(sound, sourcerun, "./*.flac", "None", -25, 2, 0.3, "yes", "English", 1.3, "Table",
                                "OverWriteData", "yes", capture_output=True)
             table = objects[0][0]
