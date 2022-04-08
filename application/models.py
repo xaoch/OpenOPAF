773c8329f4f55b5c51769b06264a83fc66d7fe88
Xavier Ochoa <xavier.ochoa@gmail.com>
2022-04-08 10:21:38 -0400

    Test Database

--- application/models.py
+++ application/models.py
@@ -11,7 +11,7 @@ def dump_datetime(value):
     """Deserialize datetime object into string form for JSON processing."""
     if value is None:
         return None
-    return [value.strftime("%Y-%m-%d"), value.strftime("%H:%M:%S")]
+    return value.strftime("%Y-%m-%d") +" "+ value.strftime("%H:%M:%S")
 
 class Presentation(db.Model):
     id = db.Column(db.Integer, primary_key=True)
